8512dd97b0c6943cb69b750669e0f28e0938b594
n0dam1 <minoru.noda.0527@gmail.com>
2017-11-24 14:04:09 +0900

    PCとモバイルのviewを出し分けするよう修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
+  include Jpmobile::ViewSelector
+  before_action :set_view_path
   protect_from_forgery with: :exception
+
+  private
+
+  def set_view_path
+    path = request.smart_phone? ? 'mobile' : 'pc'
+    prepend_view_path(Rails.root + 'app/views' + path)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  resources :posts, only: %i[index]
   root 'home#index'
 end
