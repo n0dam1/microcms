2132dc3d7dfa2590d454bc920e48bbf23aa220b9
n0dam1 <minoru.noda.0527@gmail.com>
2017-11-24 17:46:35 +0900

    メタタグ設定できるようGemを追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: Settings.site.name,
+      reverse: true, # タイトルタグ内の表記順をページタイトル|サイトタイトルの順にする
+      title: Settings.site.page_title,
+      description: Settings.site.page_description,
+      canonical: request.original_url,
+      og: default_og
+    }
+  end
+
+  private
+
+  def default_og
+    {
+      title: :title,
+      type: Settings.site.og.type,
+      url: request.original_url,
+      image: image_url(Settings.site.og.image_path),
+      site_name: Settings.site.name,
+      description: :description,
+      locale: 'ja_JP'
+    }
+  end
 end
