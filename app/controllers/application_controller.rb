09048d603f674e29950dce62f64c06bfb2ebf5b2
n0dam1 <minoru.noda.0527@gmail.com>
2017-12-12 17:13:14 +0900

    Add authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,26 @@ class ApplicationController < ActionController::Base
   before_action :set_view_path
   protect_from_forgery with: :exception
 
+  helper_method :current_user_session, :current_user
+
   private
 
   def set_view_path
     path = request.smart_phone? ? 'mobile' : 'pc'
     prepend_view_path(Rails.root + 'app/views' + path)
+  end
+
+  def current_user_session
+    return @current_user_session if defined?(@current_user_session)
+    @current_user_session = UserSession.find
+  end
+
+  def current_user
+    return @current_user if defined?(@current_user)
+    @current_user = current_user_session &. user
+  end
+
+  def require_login
+    return redirect_to sign_in_path unless current_user
   end
 end
