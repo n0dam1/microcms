e10b655f7cf0d8206299e8ab74a487f97ae26cde
n0dam1 <minoru.noda.0527@gmail.com>
2017-12-22 17:50:41 +0900

    Modify namespace

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -1,27 +1,25 @@
-class User
-  class SessionsController < ApplicationController
-    def new
-      @user_session = UserSession.new
-    end
+class User::SessionsController < ApplicationController
+  def new
+    @user_session = UserSession.new
+  end
 
-    def create
-      @user_session = UserSession.new(user_session_params)
-      if @user_session.save
-        redirect_to root_path
-      else
-        render :new
-      end
-    end
-
-    def destroy
-      current_user_session.destroy
+  def create
+    @user_session = UserSession.new(user_session_params)
+    if @user_session.save
       redirect_to root_path
+    else
+      render :new
     end
+  end
 
-    private
+  def destroy
+    current_user_session.destroy
+    redirect_to root_path
+  end
 
-    def user_session_params
-      params.require(:user_session).permit(:email, :password)
-    end
+  private
+
+  def user_session_params
+    params.require(:user_session).permit(:email, :password)
   end
 end
